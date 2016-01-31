5006d5000de28be05821d4ea4ee36bc573099106
Thaddeus Hughes <Thaddeus Hughes>
2016-01-31 11:42:59 -0500

    Calculated wall thicknesses

--- RearWingMountAnalysis.m
+++ RearWingMountAnalysis.m
@@ -25,7 +25,7 @@ PBottomRearChassis = [-61.31,14.22,10.86];
 PBottomRearChassis_Left = [PBottomRearChassis(1),PBottomRearChassis(2),-PBottomRearChassis(3)];
 
 
-PBottomFrontWing = [55.80,27.24,18.33];
+PBottomFrontWing = [-55.80,27.24,18.33];
 PBottomFrontWing_Left = [PBottomFrontWing(1),PBottomFrontWing(2),-PBottomFrontWing(3)];
 
 PBottomFrontChassis = [-60.79,12.71,9.99];
@@ -55,10 +55,10 @@ FBottomFront=LBottomFront/norm(LBottomFront) *TBottomFront;
 FBottomFront_Left=LBottomFront_Left/norm(LBottomFront_Left) *TBottomFront;
 
 % Extra Forces in lbf. NEED ESTIMATES
-FDown = [0,-200,0];
-PDown = [-60, 35, 0];
+FDown = [0,-500,0];
+PDown = [-61, 35, 0];
 
-FDrag = [-20,0,0];
+FDrag = [40,0,0];
 PDrag = [-60, 35, 0];
 
 eqs = [FDown + FUpper+FUpper_Left + FBottomRear+FBottomRear_Left + FBottomFront+FBottomFront_Left == [0,0,0],
@@ -68,7 +68,9 @@ eqs = [FDown + FUpper+FUpper_Left + FBottomRear+FBottomRear_Left + FBottomFront+
 display(TBottomRear)
 
 
-%FrontWallThickness_Yield = (solve(PipeYieldStress == TFront*FOS/PipeArea));
+UpperWallThickness_Yield = (solve(PipeYieldStress == abs(TUpper)*FOS/PipeArea));
+BottomRearWallThickness_Yield = (solve(PipeYieldStress == abs(TBottomRear)*FOS/PipeArea));
+BottomFrontWallThickness_Yield = (solve(PipeYieldStress == abs(TBottomFront)*FOS/PipeArea));
 FrontWallThickness = 0.028;
 %PipeArea = pi/4*(PipeOD^2-(PipeOD-FrontWallThickness_Yield(FrontWallThickness<0.1)*2)^2);
 %Deformation_Tierod = TFront*norm(LBottomFront)/PipeModulus/PipeArea;
@@ -76,5 +78,7 @@ FrontWallThickness = 0.028;
 fprintf('Tension in Upper Rods: %f lbs\n', double(TUpper) )
 fprintf('Tension in Bottom Rear Rods: %f lbs\n', double(TBottomRear) )
 fprintf('Tension in Bottom Front Rods: %f lbs\n', double(TBottomFront) )
-%fprintf('Required Tie Rod Wall Thickness (Yield, w. FOS): %f0.2 in\n', double(FrontWallThickness_Yield(FrontWallThickness_Yield<0.1)) )
+fprintf('Required Upper Tie Rod Wall Thickness (Yield, w. FOS): %f0.2 in\n', double(max(UpperWallThickness_Yield(UpperWallThickness_Yield<0.1))) )
+fprintf('Required Bottom Rear Tie Rod Wall Thickness (Yield, w. FOS): %f0.2 in\n', double(max(BottomRearWallThickness_Yield(BottomRearWallThickness_Yield<0.1))) )
+fprintf('Required Bottom Front Tie Rod Wall Thickness (Yield, w. FOS): %f0.2 in\n', double(max(BottomFrontWallThickness_Yield(BottomFrontWallThickness_Yield<0.1))) )
 %fprintf('Deformation at Loading (Realistic, no FOS): %f0.2 in\n\n',  double(Deformation_Tierod) ) 
